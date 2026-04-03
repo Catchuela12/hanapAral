967a93759aa8324a70fcf677d40ea9ba3ec3caf1
JohrencDeLeon <deleonjohrenc33@gmail.com>
2026-04-03 14:36:56 +0800

    Additional Files

--- app/src/main/java/com/example/hanaparal/MainActivity.kt
+++ app/src/main/java/com/example/hanaparal/MainActivity.kt
@@ -10,38 +10,61 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.hanaparal.ui.groups.CreateGroupScreen
+import com.example.hanaparal.ui.groups.GroupDetailScreen
+import com.example.hanaparal.ui.groups.GroupListScreen
 import com.example.hanaparal.ui.theme.HanapAralTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             HanapAralTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                
+                NavHost(
+                    navController = navController,
+                    startDestination = "group_list"
+                ) {
+                    composable("group_list") {
+                        GroupListScreen(
+                            onGroupClick = { groupId ->
+                                navController.navigate("group_detail/$groupId")
+                            },
+                            onCreateGroup = {
+                                navController.navigate("create_group")
+                            }
+                        )
+                    }
+                    composable("create_group") {
+                        CreateGroupScreen(
+                            onBackClick = { navController.popBackStack() },
+                            onGroupCreated = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+                    composable(
+                        route = "group_detail/{groupId}",
+                        arguments = listOf(navArgument("groupId") { type = NavType.StringType })
+                    ) { backStackEntry ->
+                        val groupId = backStackEntry.arguments?.getString("groupId") ?: ""
+                        GroupDetailScreen(
+                            groupId = groupId,
+                            onBackClick = { navController.popBackStack() }
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HanapAralTheme {
-        Greeting("Android")
-    }
-}
