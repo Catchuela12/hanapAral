4886268e7fbbf965022d230d71d0ef3e4e65bd91
Izumi <bejaydayao18@gmail.com>
2026-04-03 10:46:43 +0800

    Initialize authentication module

---
+++ app/src/main/java/com/example/hanaparal/di/AuthModule.kt
@@ -0,0 +1,19 @@
+package com.example.hanaparal.di
+
+import com.example.hanaparal.data.repository.AuthRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(): AuthRepository {
+        return AuthRepository()
+    }
+}
